08722b18874ea06003b736ae5f0e9f8f1dea96d8
Jessica Hirsch <jrachelh5@gmail.com>
2018-05-04 14:36:54 -0700

    Fix up helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  	root 'pages#home'
-	get 'welcome' => 'pages#home'
-	get 'index' => 'pages#index'
-	get 'cucumber' => 'pages#cucumber'
+  	root 'welcome#index'
 
 end
